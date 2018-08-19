397c5331aa3f08393b7c0ba916a45c49805fa77c
StoneT2000 <stoneztao@gmail.com>
2018-08-19 23:52:33 +0800

    Updated buttons.js

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -27,6 +27,15 @@ $(document).on("ready",function(){
   $("#change_bread").on("click", function(){
     breadNum = (breadNum + 1) % breads.length;
     bimg = loadImage(breads[breadNum]);
+    console.log("clear slices and reset")
+    slices = [];
+    tempSlice = [];
+    numOfSlices = 0;
+    slicing = false;
+    results = [];
+    $("#messageResponse").text("");
+    $("#numpieces").text(0);
+    $("#eveness").text("N/A")
   })
 
 })
